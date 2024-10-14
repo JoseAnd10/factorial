b3221c8a25c499d23c77f7754245c8015bc2295b
2024-2025 DAM1 <2024-2025 DAM1@DESKTOP-QBB9R8N>
2024-10-14 17:33:30 +0200

    hola soy paca la alpaca

---
+++ src/main/java/com/mycompany/circulo/Circulo.java
@@ -0,0 +1,18 @@
+package com.mycompany.circulo;
+
+public class Circulo {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    
+}
